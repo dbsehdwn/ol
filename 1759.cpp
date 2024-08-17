364a0e78855503aae5c99877dbc7d3495152cc9c
dbsehdwn <dbsehdwn12081@gmail.com>
2024-08-17 02:36:59 +0000

    1759

---
+++ 1759.cpp
@@ -0,0 +1,30 @@
+// https://www.acmicpc.net/problem/1759
+#include<bits/stdc++.h>
+using namespace std;
+int l,c,check[20];
+char ch[20],arr[20];
+void f(int idx, int b){
+    if(idx==l){
+        int cnt1=0,cnt2=0;
+        for(int i=1;i<=l;i++){
+            if(arr[i]=='a'||arr[i]=='e'||arr[i]=='i'||arr[i]=='o'||arr[i]=='u') cnt1++;
+            else cnt2++;
+        }
+        if(cnt1>=1&&cnt2>=2){
+            for(int i=1;i<=l;i++) cout<<arr[i];
+            cout<<'\n';
+        }
+        return;
+    }
+    for(int i=b+1;i<=c;i++){
+        arr[idx+1]=ch[i];
+        f(idx+1,i);
+    }
+}
+int main(){
+    cin>>l>>c;
+    for(int i=1;i<=c;i++) cin>>ch[i];
+    sort(ch+1,ch+c+1);
+    f(0,0);
+    return 0;
+}
